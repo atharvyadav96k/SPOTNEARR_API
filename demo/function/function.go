57906084f88379d182353dd4282b953bc047ef77
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-01 19:40:59 +0530

    demo gitactions

---
+++ demo/function/function.go
@@ -0,0 +1,17 @@
+// function/main.go
+package p
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("HelloWorld", helloWorld)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, Spotnearr API is live!")
+}
