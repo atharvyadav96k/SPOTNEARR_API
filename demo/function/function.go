5833f1fa53013a44fa16d3656ec8051465af0822
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-01 21:38:51 +0530

    updating fnction

--- demo/function/function.go
+++ demo/function/function.go
@@ -1,17 +1,7 @@
-// function/main.go
-package p
+package demofunction
 
-import (
-	"fmt"
-	"net/http"
+import "net/http"
 
-	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-)
-
-func init() {
-	functions.HTTP("HelloWorld", helloWorld)
-}
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, changes")
+func Demo(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
--- demo/main.tf
+++ demo/main.tf
@@ -1,37 +1,45 @@
 terraform {
   backend "gcs" {
-    bucket  = "terraform-state-603675804309"
+    bucket  = "terraform-state-603675804308"
     prefix  = "cloud-functions/demo"
   }
+
   required_providers {
-    google  = { source = "hashicorp/google" }
-    archive = { source = "hashicorp/archive" }
-    random  = { source = "hashicorp/random" }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
-variable "project_id"      { type = string }
-variable "region"          { type = string }
-variable "service_account" { type = string }
+variable "project_id" {
+  type = string
+}
+
+variable "project_number" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "service_account" {
+  type = string
+}
 
 provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-# 1. Automatically zip the 'function' folder
-data "archive_file" "source" {
-  type        = "zip"
-  source_dir  = "${path.module}/function"
-  output_path = "${path.module}/source.zip"
-}
-
-# 2. Generate a random suffix for the bucket name
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-# 3. Create the source bucket with required uniform access
 resource "google_storage_bucket" "source_bucket" {
   name                        = "${var.project_id}-function-${random_id.bucket_suffix.hex}"
   location                    = var.region
@@ -39,22 +47,20 @@ resource "google_storage_bucket" "source_bucket" {
   force_destroy               = true
 }
 
-# 4. Upload the zip (using the hash of the file for the name)
 resource "google_storage_bucket_object" "source_archive" {
-  # This uses the hash of the actual content to trigger a redeploy when code changes
-  name   = "source-${data.archive_file.source.output_md5}.zip"
+  name   = "source-${filesha256("source.zip")}.zip"
   bucket = google_storage_bucket.source_bucket.name
-  source = data.archive_file.source.output_path
+  source = "source.zip"
 }
 
-# 5. The Cloud Function
-resource "google_cloudfunctions2_function" "helloworld" {
-  name     = "HelloWorld"
+
+resource "google_cloudfunctions2_function" "demo" {
+  name     = "demo"
   location = var.region
 
   build_config {
-    runtime     = "go122" 
-    entry_point = "HelloWorld"
+    runtime     = "go121"
+    entry_point = "Demo"
     service_account = "projects/${var.project_id}/serviceAccounts/${var.service_account}"
     source {
       storage_source {
@@ -66,20 +72,20 @@ resource "google_cloudfunctions2_function" "helloworld" {
 
   service_config {
     max_instance_count    = 1
-    available_memory      = "256Mi"
+    available_memory      = "256M"
+    timeout_seconds       = 60
     service_account_email = var.service_account
     ingress_settings      = "ALLOW_ALL"
   }
 }
 
-# 6. Make the function public
 resource "google_cloud_run_service_iam_member" "public_access" {
   location = var.region
-  service  = google_cloudfunctions2_function.helloworld.name
+  service  = google_cloudfunctions2_function.demo.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
 output "function_url" {
-  value = google_cloudfunctions2_function.helloworld.service_config[0].uri
+  value = google_cloudfunctions2_function.demo.service_config[0].uri
 }
