ae822b423bbad89cb729d464d6b461c110e1b7cc
Atharv Yadv <atharvyadav96k@gmail.com>
2026-05-04 23:41:37 +0530

    fix: correct variable reference for source archive name in main.tf

--- auth/user/login/main.tf
+++ auth/user/login/main.tf
@@ -56,7 +56,7 @@ resource "google_storage_bucket" "source_bucket" {
 }
 
 resource "google_storage_bucket_object" "source_archive" {
-  name   = "${function_name.values}-${filesha256("source.zip")}.zip"
+  name   = "${var.function_name}-${filesha256("source.zip")}.zip"
   bucket = google_storage_bucket.source_bucket.name
   source = "source.zip"
 }
