4a4debea29f53c6bbe1f3a0f1e7f920bad076122
Atharv Yadv <atharvyadav96k@gmail.com>
2026-05-05 09:07:26 +0530

    fix: update entry point for GetCategories function in main.tf

--- business/categories/get_categories/main.tf
+++ business/categories/get_categories/main.tf
@@ -66,7 +66,7 @@ resource "google_cloudfunctions2_function" "function" {
 
   build_config {
     runtime         = "go122"
-    entry_point     = "AddCategories"
+    entry_point     = "GetCategories"
     service_account = "projects/${var.project_id}/serviceAccounts/${var.service_account}"
     source {
       storage_source {
