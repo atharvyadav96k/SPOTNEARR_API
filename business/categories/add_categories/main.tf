bd4b678019227ed68ffd14f1f317eb6ea11deb03
Atharv Yadv <atharvyadav96k@gmail.com>
2026-05-05 08:22:19 +0530

    fix: update service reference in IAM member resource for cloud function

--- business/categories/add_categories/main.tf
+++ business/categories/add_categories/main.tf
@@ -87,9 +87,10 @@ resource "google_cloudfunctions2_function" "function" {
 
 resource "google_cloud_run_service_iam_member" "public_access" {
   location = var.region
-  service  = google_cloudfunctions2_function.function.name
+  service  = google_cloudfunctions2_function.function.service_config[0].service
   role     = "roles/run.invoker"
   member   = "allUsers"
+  depends_on = [google_cloudfunctions2_function.function]
 }
 
 output "function_url" {
