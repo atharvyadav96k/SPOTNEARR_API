b22b1e08ba87bd41825a8913fd06e6a80f070c99
Atharv Yadv <atharvyadav96k@gmail.com>
2026-05-08 22:43:59 +0530

    feat: add spotlight functions for followed businesses, liked spotlights, and location-based retrieval

--- users/spotlights/followed_business_spotlights/function/function.go
+++ users/spotlights/followed_business_spotlights/function/function.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 )
 
-func SpotlightsFollowedBusiness(w http.ResponseWriter, r *http.Request) {
+func Function(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	response := map[string]string{
-		"message": "Login successful",
+		"message": "Spotlights retrieval successfully",
 	}
 
 	json.NewEncoder(w).Encode(response)
--- users/spotlights/get_liked_spotlights/function/function.go
+++ users/spotlights/get_liked_spotlights/function/function.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func SpotlightLikedBusiness(w http.ResponseWriter, r *http.Request) {
+func Function(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
