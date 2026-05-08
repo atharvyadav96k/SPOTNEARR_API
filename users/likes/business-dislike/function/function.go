b0525d5566aed711b42ae24dccd3c715351a1de1
Atharv Yadv <atharvyadav96k@gmail.com>
2026-05-08 22:14:28 +0530

    fix: unify function names and entry points for business like/dislike functions

--- users/likes/business-dislike/function/function.go
+++ users/likes/business-dislike/function/function.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func DiskLikeBusiness(w http.ResponseWriter, r *http.Request) {
+func Function(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
