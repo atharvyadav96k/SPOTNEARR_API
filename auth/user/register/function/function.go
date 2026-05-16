e9351c592694621110e79ffaa98de35008a926a8
Atharv Yadv <atharvyadav96k@gmail.com>
2026-05-16 14:35:24 +0530

    refactor: rename user-register function to unify entry point naming

--- auth/user/register/function/function.go
+++ auth/user/register/function/function.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func UserRegister(w http.ResponseWriter, r *http.Request) {
+func Function(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	response := map[string]string{
