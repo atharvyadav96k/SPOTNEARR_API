2dfe746043cb7b2c5c01a1b144ef665419437d8a
Atharv Yadv <atharvyadav96k@gmail.com>
2026-05-16 23:39:48 +0530

    feat(location): add create, delete, and update functions for business locations
    - Implemented create_location function with Terraform configuration and Go code.
    - Added delete_location function to handle business location deletions.
    - Introduced update_location function to update existing business locations.
    - Created necessary Terraform files for each function, including variable definitions and resource configurations.
    - Added Go modules and sum files for dependency management in each function.
    - Established application layer initialization for database connections in each function.

---
+++ business/location/delete_location/function/function.go
@@ -0,0 +1,24 @@
+package location
+
+import (
+	"net/http"
+
+	"github.com/atharvyadav96k/SPOTNEARR_API/location/delete_location/applayer"
+	"github.com/atharvyadav96k/spotnearr-gcp/app/database/models"
+	"github.com/atharvyadav96k/spotnearr-gcp/app/utils"
+)
+
+func Function(w http.ResponseWriter, r *http.Request) {
+	businessLocation, err := utils.ParseBody[models.BusinessLocation](r)
+	if err != nil {
+		utils.BadRequest(w, err.Error())
+		return
+	}
+	app := applayer.Init()
+	err = app.DeleteBusinessLocation(businessLocation.ID)
+	if err != nil {
+		utils.BadRequest(w, err.Error())
+		return
+	}
+	utils.Created(w, "business location created successfully", nil)
+}
